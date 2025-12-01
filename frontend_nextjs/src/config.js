16a5cfbf56df325b43fbe4acb1356e361dba421c
bubu139 <giakhanhtran88@gmail.com>
2025-12-01 15:55:08 +0700

    Delete frontend_nextjs/src directory

--- frontend_nextjs/src/config.js
+++
@@ -1,6 +0,0 @@
-// Ưu tiên NEXT_PUBLIC_API_BASE_URL; nếu chưa đặt sẽ rơi về NEXT_PUBLIC_API_URL hoặc localhost để tránh crash
-const FALLBACK_API_BASE_URL = 'http://localhost:8000';
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ||
-  process.env.NEXT_PUBLIC_API_URL ||
-  FALLBACK_API_BASE_URL;
