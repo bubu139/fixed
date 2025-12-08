86f731f165db033cf149f90437b4baf05993418d
bubu139 <giakhanhtran88@gmail.com>
2025-12-08 19:07:00 +0700

    Delete frontend_nextjs/src directory

--- frontend_nextjs/src/lib/supabaseClient.ts
+++
@@ -1,9 +0,0 @@
-// frontend_nextjs/src/lib/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js';
-
-// Lấy URL và ANON KEY từ biến môi trường
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-// Tạo client Supabase
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
